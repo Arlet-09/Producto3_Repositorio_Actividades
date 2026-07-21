6b11ab45b9773761429b12188a72faca9982533a
Luz Arlet González Claudio <2311080440@alumno.utpuebla.edu.mx>
2026-07-20 20:33:24 -0600

    Viernes 03/07/2026: Cliente .NET para grpc
    Crear y probar clientes para el servicio grpc: http://localhost:5159 en .NET, Java, Go y Node.js.

---
+++ clientenet/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using Greet; // El namespace generado a partir del paquete 'greet' en el proto
+
+// Como el servidor local no usa HTTPS por defecto en esta plantilla, permitimos HTTP inseguro
+using var channel = GrpcChannel.ForAddress("http://localhost:5077");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Juan desde .NET" });
+Console.WriteLine("Respuesta del servidor: " + reply.Message);
